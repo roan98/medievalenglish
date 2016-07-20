988c05c0f693012088207d9a2a2ab399afb18393
Roan van Adrichem <roanvanadrichem98@live.nl>
2016-07-20 19:37:07 +0200

    nu dan

---
+++ medieval_english.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+
+<head>
+ <title> Medieval English </title>
+ <link rel="stylesheet" type="text/css" href="stylesheet.css" />
+</head>
+
+<body background="images/marble3.jpg"> <div class="inhoud">
+  <h1 class="medieval"> Manuscript </h1>
+   <h3 class="universe"> Explore the Universe </h3>
+    <ul class="menu">
+     <li><a href="medieval_english.html">Medieval English</a></li>
+     <li><a href="Mandarin.html">Mandarin</a></li>
+     <li><a href="link">New vulcan</a></li>
+     <li><a href="link">Cardassian</a></li>
+    </ul>
+      
+<div class="content">
+ <h2 class="header"> Voyage </h2>
+  <p class="me">80 days around the world, we'll find a pot of gold just sitting where the rainbow's ending. Time - we'll fight against the time, and we'll fly on the white wings of the wind. 80 days around the world, no we won't say a word before the ship is really back. Round, round, all around the world. Round, all around the world. Round, all around the world. Round, all around the world.</p>
+
+  <p class="me">I never spend much time in school but I taught ladies plenty. It's true I hire my body out for pay, hey hey. I've gotten burned over Cheryl Tiegs, blown up for Raquel Welch. But when I end up in the hay it's only hay, hey hey. I might jump an open drawbridge, or Tarzan from a vine. 'Cause I'm the unknown stuntman that makes Eastwood look so fine.</p>
+  
+	<img class="divider" src="images/divider.png">
+	
+ <h2> One and all </h2>
+  <p class="me">One for all and all for one, Muskehounds are always ready. One for all and all for one, helping everybody. One for all and all for one, it's a pretty story. Sharing everything with fun, that's the way to be. One for all and all for one, Muskehounds are always ready. One for all and all for one, helping everybody. One for all and all for one, can sound pretty corny. If you've got a problem chum, think how it could be.</p>
+
+  <p class="me">Just the good ol' boys, never meanin' no harm. Beats all you've ever saw, been in trouble with the law since the day they was born. Straight'nin' the curve, flat'nin' the hills. Someday the mountain might get 'em, but the law never will. Makin' their way, the only way they know how, that's just a little bit more than the law will allow. Just good ol' boys, wouldn't change if they could, fightin' the system like a true modern day Robin Hood.</p>
+	<img class="divider" src="images/divider.png">
+ 
+	</div>  
+ </div>  
+</body>
+<p class="knight">
+ Knight Rider, a shadowy flight into the dangerous world of a man who does not exist. Michael Knight, a young loner on a crusade to champion the cause of the innocent, the helpless in a world of criminals who operate above the law. read more...
+  </p>
+</html>
